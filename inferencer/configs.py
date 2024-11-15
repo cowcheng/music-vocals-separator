053d08c5082885ff8a5e4e1fe79dcdd8508330d9
cowcheng <cowcheng0@gmail.com>
2024-11-15 13:03:01 +0800

    update models weight path

--- inferencer/configs.py
+++ inferencer/configs.py
@@ -7,13 +7,13 @@ LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 LOG_DATEFMT = "%Y-%m-%d %H:%M:%S"
 LOG_LEVEL = logging.INFO
 
-MDX_C_MODEL_PATH = f"{ROOT_DIR}/inferencer/weights/mdx_c/MDX23C_D1581.ckpt"
+MDX_C_MODEL_PATH = f"{ROOT_DIR}/inferencer/weights/MDX23C_D1581.ckpt"
 MDX_C_MODEL_CONFIGS_PATH = (
     f"{ROOT_DIR}/inferencer/configs/mdx_c/model_2_stem_061321.yaml"
 )
 
 VR_DE_ECHO_MODEL_PATH = (
-    f"{ROOT_DIR}/inferencer/weights/vr_de_echo/UVR-De-Echo-Aggressive.pth"
+    f"{ROOT_DIR}/inferencer/weights/UVR-De-Echo-Aggressive.pth"
 )
 VR_DE_ECHO_CONFIGS_PATH = f"{ROOT_DIR}/inferencer/configs/vr/de_echo.yaml"
 VR_DE_ECHO_MODELS_PARAMS_PATH = f"{ROOT_DIR}/inferencer/configs/vr/4band_v3.json"
